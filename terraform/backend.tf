6d0467d4e5e93ec92e5c9534531d460864e27dde
PhalePallo <160241510+PhalePallo@users.noreply.github.com>
2026-01-20 15:56:59 +0200

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "aws-cloud-governance-tfstate-659587495811"
-    key            = "state/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "aws-cloud-governance-tf-locks"
-    encrypt        = true
-  }
-}
